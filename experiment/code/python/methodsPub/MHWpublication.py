f4689dd1cedce223835dc552f06b0ea5a2a344d6
47021807+ameliaritger@users.noreply.github.com <47021807+ameliaritger@users.noreply.github.com>
2024-03-08 17:23:07 -0800

    add code for threshold cycling (but probably don't ultimately keep)

--- experiment/code/python/methodsPub/MHWpublication.py
+++ experiment/code/python/methodsPub/MHWpublication.py
@@ -11,6 +11,8 @@ import CleanUp as clean
 #heater set to 25/20, 20/18, 18/16 is TOO HOT
 #10:30AM 5/8, chiller increased from 10/14C to 13/15C
 #12:15 5/8, chiller increased to 57/59
+#2:45 changed temp comparison to 0.04 threshold rather than "too hot/too cold"
+#4:10 reduced to 0.01 threshold
 #next step, reduce to only 2 tanks and pump chilled water to heating tank
 
 #min temp is 58F, max temp is 60F
@@ -69,18 +71,23 @@ def mhw_sim():
             avg_temps[:] = [round(num, 2) for num in avg_temps] #round avg tank temperatures to 2 decimal places
             hot_set = temp_set[0]
             temp_set = [hot_set]
-            if avg_temps[0] > temp_set[0]: #if the tank is too hot
+            if abs(avg_temps[0]-temp_set[0]) <= 0.01: #if the tank is within 0.05C away from the set point
+                print(f"Tank perfect temperature!")
+                io_inst.heat(0, 0) #turn the cold pump off
+                io_inst.heat(1, 0) #turn the hot pump off
+                heater_status.append("off")
+            elif (avg_temps[0]-temp_set[0])>0.01: #if the tank is too hot
                 print(f"Tank too hot!")
                 io_inst.heat(1, 0) #turn the hot pump off
                 io_inst.heat(0, 1) #turn the cold pump on
                 heater_status.append("off")
-            elif avg_temps[0] < temp_set[0]: #if the tank is too cold
+            elif (avg_temps[0] - temp_set[0])<-0.01: #if the tank is too cold
                 print(f"Tank too cold!")
                 io_inst.heat(0, 0) #turn the cold pump off
                 io_inst.heat(1, 1) #turn the hot pump on
                 heater_status.append("on")
             else: #if the temperature is just right
-                print(f"Tank perfect temperature!")
+                print(f"WHAT IS GOING ON")
                 io_inst.heat(0, 0) #turn the cold pump off
                 io_inst.heat(1, 0) #turn the hot pump off
                 heater_status.append("off")
